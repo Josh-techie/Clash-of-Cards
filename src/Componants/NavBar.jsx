d8bac42084639ff92501b93d9a2b1e626a56a410
Joe-tech <95583855+Josh-techie@users.noreply.github.com>
2023-11-28 13:19:20 +0100

    Responsive navbar

--- src/Componants/NavBar.jsx
+++ src/Componants/NavBar.jsx
@@ -1,53 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
 
-
 function NavBar() {
+  const [isNavOpen, setNavOpen] = useState(false);
+
+  const handleToggle = () => {
+    setNavOpen(!isNavOpen);
+  };
+
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mx-auto">
-  <Link class="navbar-brand ml-auto" to="/">
-    <img src="Assets/logo.png" alt="Logo" width="100" height="100" />
-  </Link>
-  <button
-    class="navbar-toggler"
-    type="button"
-    data-toggle="collapse"
-    data-target="#navbarNav"
-    aria-controls="navbarNav"
-    aria-expanded="false"
-    aria-label="Toggle navigation"
-  >
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-        <Link class="nav-link" to="/home">
-          Overview
-        </Link>
-      </li>
-      <li class="nav-item">
-        <Link class="nav-link" to="/features">
-          Features
-        </Link>
-      </li>
-      <li class="nav-item">
-        <Link class="nav-link" to="/getting-started">
-          Getting Started
-        </Link>
-      </li>
-      <li class="nav-item">
-        <Link class="nav-link" to="/contact-us">
-          Contact Us
-        </Link>
-      </li>
-    </ul>
-  </div>
-</nav>
-
-
-
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark mx-auto">
+      <Link className="navbar-brand ml-auto" to="/">
+        <img src="Assets/logo.png" alt="Logo" width="100" height="100" />
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        onClick={handleToggle}
+        aria-expanded={isNavOpen ? 'true' : 'false'}
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className={`collapse navbar-collapse ${isNavOpen ? 'show' : ''}`} id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/home">
+              Overview
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/features">
+              Features
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/getting-started">
+              Getting Started
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/contact-us">
+              Contact Us
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
