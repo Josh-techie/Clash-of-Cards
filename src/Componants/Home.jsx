b4acfec758ad2d77f41d3d462618e27eee6ec16c
Joe-tech <95583855+Josh-techie@users.noreply.github.com>
2023-11-29 18:45:57 +0100

    Slider first imporvement

--- src/Componants/Home.jsx
+++ src/Componants/Home.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 import { RxDotFilled } from 'react-icons/rx';
-import './Home.css';
+import './Home.css'; // Make sure to import your custom styles
 import slider1 from '../Slides/slide-1.jpg';
 import slider2 from '../Slides/slide-2.jpg';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
-
 
 function Home() {
   const slides = [
@@ -16,7 +15,6 @@ function Home() {
     {
       url: slider2,
     },
-
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -37,34 +35,43 @@ function Home() {
     setCurrentIndex(slideIndex);
   };
 
-  const bgImage = '../Assets/bg.png';
+  const bgImage = 'Assets/bg.png';
 
   return (
-    <div style={{ backgroundImage: `url(${bgImage})`, backgroundSize: 'cover', height: '100vh' }}>
-      <img
-        src={slides[currentIndex].url}
-        alt={`Slide ${currentIndex + 1}`}
-        className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
-      />
+    <div className="container-fluid d-flex align-items-center" style={{ backgroundImage: `url(${bgImage})`, backgroundSize: 'cover', height: '100vh' }}>
+      {/* Left side with text */}
+      <div className="w-25 p-4">
+        <h1 className="text-4xl font-bold mb-4">Your Title Here</h1>
+        <p className="text-lg">Your text content goes here.</p>
+      </div>
 
-      {/* Left Arrow */}
-      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-        <BsChevronCompactLeft onClick={prevSlide} size={30} />
-      </div>
-      {/* Right Arrow */}
-      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-        <BsChevronCompactRight onClick={nextSlide} size={30} />
-      </div>
-      <div className='flex top-4 justify-center py-2'>
-        {slides.map((slide, slideIndex) => (
-          <div
-            key={slideIndex}
-            onClick={() => goToSlide(slideIndex)}
-            className='text-2xl cursor-pointer'
-          >
-            <RxDotFilled />
-          </div>
-        ))}
+      {/* Right side with slider */}
+      <div className="w-75 relative">
+        <img
+          src={slides[currentIndex].url}
+          alt={`Slide ${currentIndex + 1}`}
+          className='w-100 h-100 rounded-2xl bg-center bg-cover duration-500'
+        />
+
+        {/* Left Arrow */}
+        <div className='absolute top-50% -translate-y-50% left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+          <BsChevronCompactLeft onClick={prevSlide} size={30} />
+        </div>
+        {/* Right Arrow */}
+<div className='absolute top-50% -translate-y-50% right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+          <BsChevronCompactRight onClick={nextSlide} size={30} />
+        </div>
+        <div className='flex top-4 justify-center py-2 absolute right-0 left-0 mx-auto'>
+          {slides.map((slide, slideIndex) => (
+            <div
+              key={slideIndex}
+              onClick={() => goToSlide(slideIndex)}
+              className='text-2xl cursor-pointer'
+            >
+              <RxDotFilled />
+            </div>
+          ))}
+        </div>
       </div>
 
       {/* Uncomment the following lines to test the login and sign-up forms */}
