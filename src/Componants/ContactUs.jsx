42bfb64330a741c0d24b3a6daaf82f5efa15ee6b
Joe-tech <95583855+Josh-techie@users.noreply.github.com>
2023-11-29 14:05:05 +0100

    Added fontawesome
    I also added a flow effect like snow

--- src/Componants/ContactUs.jsx
+++ src/Componants/ContactUs.jsx
@@ -1,11 +1,117 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+const formStyles = {
+  maxWidth: '400px',
+  margin: '0 auto',
+  padding: '20px',
+  boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+  borderRadius: '5px',
+};
+
+const labelStyles = {
+  display: 'block',
+  margin: '10px 0',
+  fontSize: '16px',
+};
+
+const inputStyles = {
+  width: '100%',
+  padding: '10px',
+  fontSize: '16px',
+  margin: '5px 0 15px 0',
+  boxSizing: 'border-box',
+  borderRadius: '3px',
+  border: '1px solid #ccc',
+};
+
+const textareaStyles = {
+  width: '100%',
+  padding: '10px',
+  fontSize: '16px',
+  margin: '5px 0 15px 0',
+  boxSizing: 'border-box',
+  borderRadius: '3px',
+  border: '1px solid #ccc',
+};
+
+const buttonStyles = {
+  backgroundColor: '#4CAF50',
+  color: 'white',
+  padding: '15px 20px',
+  fontSize: '16px',
+  borderRadius: '3px',
+  border: 'none',
+  cursor: 'pointer',
+};
 
 function ContactUs() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can handle form submission logic here
+    console.log('Form submitted:', formData);
+    // Reset the form after submission
+    setFormData({ name: '', email: '', message: '' });
+  };
+
   return (
-    <div>
-      <h2> This is the contact us page. </h2>
+    <div style={formStyles}>
+      <h2>Contact Us</h2>
+      <form onSubmit={handleSubmit}>
+        <label style={labelStyles} htmlFor="name">
+          Name:
+        </label>
+        <input
+          style={inputStyles}
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+
+        <label style={labelStyles} htmlFor="email">
+          Email:
+        </label>
+        <input
+          style={inputStyles}
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+
+        <label style={labelStyles} htmlFor="message">
+          Message:
+        </label>
+        <textarea
+          style={textareaStyles}
+          id="message"
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+          required
+        ></textarea>
+
+        <button style={buttonStyles} type="submit">
+          Submit
+        </button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default ContactUs
+export default ContactUs;
--- src/Componants/Home.jsx
+++ src/Componants/Home.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFire } from '@fortawesome/free-solid-svg-icons';
+import './Home.css';
+
+class Everything extends React.Component {
+  render() {
+    let snowflakes = [];
+    for (var i = 0; i < 40; i++) {
+      const position = Math.random() * 100;
+      const delay = Math.random() * 2.8;
+      const style = {
+        animationDelay: delay + 's',
+        left: position + '%',
+      };
+      snowflakes.push(
+        <div key={i} style={style} className="snowflakeContainer">
+          {/* Use FontAwesomeIcon for the fire icon */}
+          <FontAwesomeIcon icon={faFire} className="snowflake" />
+        </div>
+      );
+    }
+    return (
+      <div className="everything">
+        {snowflakes}
+        <div className="ground"></div>
+      </div>
+    );
+  }
+}
 
 function Home() {
   return (
     <div>
-      <h2> This is the home page </h2>
+      <h2>This is the home page</h2>
+      <Everything />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
