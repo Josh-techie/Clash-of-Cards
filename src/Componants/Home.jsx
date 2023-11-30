dfc7601d72d5fd330cc6526c5f86e5ed1064b1e3
Joe-tech <95583855+Josh-techie@users.noreply.github.com>
2023-11-30 18:37:22 +0100

    Slider fixed

--- src/Componants/Home.jsx
+++ src/Componants/Home.jsx
@@ -1,7 +1,9 @@
+// Home.jsx
+
 import React, { useState } from 'react';
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 import { RxDotFilled } from 'react-icons/rx';
-import './Home.css'; // Make sure to import your custom styles
+import './Home.css'; // Import your external CSS file
 import slider1 from '../Slides/slide-1.jpg';
 import slider2 from '../Slides/slide-2.jpg';
 
@@ -39,33 +41,33 @@ function Home() {
     <div className="container-fluid d-flex align-items-center" style={{ backgroundImage: `url(${bgImage})`, backgroundSize: 'cover', height: '100vh' }}>
       {/* Left side with text */}
       <div className="w-100 p-4 text-container">
-        <h1 className="display-4 font-weight-bold mb-4">Your Title Here</h1>
-        <p className="lead">Your text content goes here.</p>
+        <h1 className="display-title">Your Title Here</h1>
+        <p className="lead-text">Your text content goes here.</p>
       </div>
 
       {/* Right side with slider */}
-      <div className="w-70 pr-5 pb-5 position-relative">
+      <div className="slider-container">
         <img
           src={slides[currentIndex].url}
           alt={`Slide ${currentIndex + 1}`}
-          className='w-100 h-100 rounded-2xl bg-center bg-cover duration-500 slider-image'
+          className='w-100 h-100 rounded-2xl slider-image'
         />
 
         {/* Left Arrow */}
-        <div className='position-absolute top-50% start-0 translate-middle-y text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-          <BsChevronCompactLeft onClick={prevSlide} size={20} />
+        <div className='arrow-container left-arrow'>
+          <BsChevronCompactLeft className='arrow-button' onClick={prevSlide} />
         </div>
         {/* Right Arrow */}
-        <div className='position-absolute top-50% end-0 translate-middle-y text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-          <BsChevronCompactRight onClick={nextSlide} size={20} />
+        <div className='arrow-container right-arrow'>
+          <BsChevronCompactRight className='arrow-button' onClick={nextSlide} />
         </div>
         {/* Dots */}
-        <div className='d-flex justify-content-center align-items-center position-absolute bottom-0 start-50 translate-middle-x py-2'>
+        <div className='dots-container'>
           {slides.map((slide, slideIndex) => (
             <div
               key={slideIndex}
               onClick={() => goToSlide(slideIndex)}
-              className={`text-2xl cursor-pointer mx-1 ${slideIndex === currentIndex ? 'text-white' : 'text-black'}`}
+              className={`dot ${slideIndex === currentIndex ? 'active' : ''}`}
             >
               <RxDotFilled />
             </div>
